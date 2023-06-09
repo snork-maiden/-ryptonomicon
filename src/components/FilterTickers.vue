e4f258a5062ec844a08ec98fbc949e67e55d44e9
snork-maiden <pollykoldysheva@gmail.com>
2023-06-09 15:05:13 +0400

    Add pagination

--- src/components/FilterTickers.vue
+++ src/components/FilterTickers.vue
@@ -10,6 +10,7 @@ export default {
     return {
       filter: "",
       page: 1,
+      filteredTickers: this.tickers,
     };
   },
   computed: {
@@ -20,47 +21,62 @@ export default {
     endIndex() {
       return this.page * 6;
     },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
   },
   methods: {
     getFilteredList() {
       this.filter = this.filter.toUpperCase();
 
-      const filteredTickers = this.tickers.filter((ticker) =>
+      this.filteredTickers = this.tickers.filter((ticker) =>
         ticker.name.includes(this.filter)
       );
-      this.$emit("filterTickers", filteredTickers);
+      this.$emit("filterTickers", this.paginatedTickers);
     },
   },
   watch: {
     tickers() {
       this.getFilteredList();
     },
+    filter() {
+      this.page = 1;
+      this.getFilteredList();
+    },
+    page() {
+      this.$emit("filterTickers", this.paginatedTickers);
+    },
+  },
+  created() {
+    this.$emit("filterTickers", this.paginatedTickers);
   },
 };
 </script>
 <template>
   <div>
-    <!-- <button
-      class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      v-if="page > 1"
-      @click="page -= 1"
-    >
-      Назад
-    </button>
-    <button
-      class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      @click="page += 1"
-      v-if="hasNextPage"
-    >
-      Вперед
-    </button> -->
+    <template v-if="hasNextPage || page > 1">
+      <button
+        class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        :disabled="page === 1"
+        @click="page -= 1"
+      >
+        Назад
+      </button>
+      <button
+        class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        @click="page += 1"
+        :disabled="!hasNextPage"
+      >
+        Вперед
+      </button>
+    </template>
     <div>
       Фильтр:
-      <input
-        v-model="filter"
-        @input="getFilteredList"
-        class="p-1 ml-2 focus:outline-yellow-500"
-      />
+      <input v-model="filter" class="p-1 ml-2 focus:outline-yellow-500" />
     </div>
   </div>
 </template>
