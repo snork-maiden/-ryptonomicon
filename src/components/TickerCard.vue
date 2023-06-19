7f4ab31049535eaaccdf25a6578dcb2ccb36ad4f
snork-maiden <pollykoldysheva@gmail.com>
2023-06-19 11:36:07 +0400

    Add validation

--- src/components/TickerCard.vue
+++ src/components/TickerCard.vue
@@ -6,24 +6,37 @@ export default {
   props: {
     ticker: Object,
   },
+  methods: {
+    formatPrice(price) {
+      if (!price) return "-";
+      return price;
+      // return price > 1 ? price.toFixed(2) : price.toPrecision(2);
+    },
+  },
 };
 </script>
 <template>
   <div
-    class="bg-white overflow-hidden shadow rounded-lg outline-yellow-500 outline-offset-2 outline-4 cursor-pointer"
+    class="bg-white overflow-hidden shadow rounded-lg outline-yellow-500 outline-offset-1 outline-4 cursor-pointer"
+    :class="{
+      'shadow-red-400': !ticker.price,
+    }"
   >
     <div class="px-4 py-5 sm:p-6 text-center">
       <dt class="text-sm font-medium text-gray-500 truncate">
         {{ ticker.name }} - USD
       </dt>
       <dd class="mt-1 text-3xl font-semibold text-gray-900">
-        {{ ticker.price }}
+        {{ formatPrice(ticker.price) }}
       </dd>
     </div>
     <div class="w-full border-t border-gray-200"></div>
     <button
       @click.stop="$emit('delete')"
       class="flex items-center justify-center font-medium w-full bg-lime-700 px-4 py-4 sm:px-6 text-md text-lime-100 hover:opacity-90 transition-all focus:outline-yellow-500"
+      :class="{
+        'bg-red-800': !ticker.price
+      }"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
