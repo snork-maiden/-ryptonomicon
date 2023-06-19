5a1ef9b7bf6d37de385541aec2328eefd0fd93a6
snork-maiden <pollykoldysheva@gmail.com>
2023-06-19 11:36:48 +0400

    Improve graph

--- src/components/TickerGraph.vue
+++ src/components/TickerGraph.vue
@@ -1,30 +1,50 @@
 <script>
-import ClearButton from "./ClearButton.vue";
-
 export default {
   name: "TickerGraph",
-  components: {
-    ClearButton,
-  },
   props: {
     selectedTicker: Object,
-    graph: Array
+    graph: Array,
   },
   emits: {
     clearTicker: null,
   },
+  data() {
+    return {
+      maxGraphElements: 1,
+    };
+  },
+  mounted() {
+    this.calculateMaxGraphElements();
+    window.addEventListener("resize", this.calculateMaxGraphElements);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.calculateMaxGraphElements);
+  },
+  computed: {},
 
-  methods: {},
-  computed: {
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
+  methods: {
+    normalizeGraph(graph) {
+      if (graph.length > this.maxGraphElements) {
+        const extra = graph.length - this.maxGraphElements;
+        graph = graph.slice(extra);
+      }
+      const maxValue = Math.max(...graph);
+      const minValue = Math.min(...graph);
 
-      if (maxValue === minValue) return this.graph.map(() => 50);
+      if (maxValue === minValue) return graph.map(() => 50);
 
-      return this.graph.map(
+      return graph.map(
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
+    },
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) {
+        return;
+      }
+
+      const MinGraphElementWidth = 20;
+      const graphWidth = this.$refs.graph.clientWidth;
+      this.maxGraphElements = Math.floor(graphWidth / MinGraphElementWidth);
     },
   },
 };
@@ -34,14 +54,16 @@ export default {
     <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
       {{ selectedTicker.name }} - USD
     </h3>
-    <div class="flex items-end border-gray-600 border-b border-l h-64">
+    <div
+      class="flex items-end border-gray-600 border-b border-l h-64"
+      ref="graph"
+    >
       <div
-        v-for="(bar, idx) in normalizedGraph"
+        v-for="(bar, idx) in normalizeGraph(graph)"
         :key="idx"
         :style="{ height: `${bar}%` }"
         class="bg-purple-800 border w-8"
       ></div>
     </div>
-    <ClearButton @click="clearTicker" class="absolute"> </ClearButton>
   </section>
 </template>
